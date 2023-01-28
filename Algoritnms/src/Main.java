31a907409101667f664daa4c709b34d57e9b664f
stangaan <schtengauer@gmail.com>
2023-01-28 21:51:13 +0100

    Houmwork 1 Algorithms

---
+++ Algoritnms/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+int numOne = scanner.nextInt();
+int numTwo = scanner.nextInt();
+int numThree = scanner.nextInt();
+int sum = numOne + numTwo + numThree;
+
+
+        System.out.println(sum);
+    }
+}
